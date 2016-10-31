f4b142141dc21d3eebc08c89c2462bf5a3aa9fc3
mgaydash <mgaydash@dataforma.com>
2016-10-31 16:08:05 -0400

    Add cal create logic

--- public/javascripts/calendar.js
+++ public/javascripts/calendar.js
@@ -1,7 +1,61 @@
-( function () {
+$( function () {
   "use strict";
 
   var elements = {};
   elements.cell = $('.item').detach();
   elements.row = $('.container').detach();
-} )();
+
+  var createWeekRow = function () {
+    var i;
+    var row = elements.row.clone();
+
+    for ( i = 0; i < 7; i++ ) {
+      row.append( elements.cell.clone() );
+    }
+
+    return row;
+  };
+
+  var draw = function () {
+    var date = createVisualStartDate();
+    var month = new Date().getMonth();
+    var i;
+
+    while ( date.getMonth() <= month ) {
+      $( 'body' ).append( createWeekRow() );
+      date.setDate( date.getDate() + 7 );
+    }
+  };
+
+  // Returns the first date to be drawn by the calendar
+  var createVisualStartDate = function ( date ) {
+    date = date || new Date();
+
+    date.setDate( 1 );
+
+    while ( date.getDay() !== 0 ) {
+      date.setDate( date.getDate() - 1 );
+    }
+
+    return date;
+  };
+
+  // Returns first date that should NOT be drawn by the calendar
+  // We can do a < comparison.
+  var createVisualEndDate = function ( date ) {
+    date = date || new Date();
+    var month = date.getMonth();
+
+    date.setDate( 28 );
+    while ( date.getMonth() === month ) {
+      date.setDate( date.getDate() + 1 );
+    }
+    while ( date.getDay() !== 0 ) {
+      date.setDate( date.getDate() + 1 );
+    }
+
+    return date;
+  };
+
+  draw();
+} );
