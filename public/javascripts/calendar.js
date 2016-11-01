7930a8dd0a8ee5a27dd1d1bae17deb8cb934bcd1
mgaydash <mgaydash@dataforma.com>
2016-11-01 10:05:03 -0400

    Remove unused end date calc function

--- public/javascripts/calendar.js
+++ public/javascripts/calendar.js
@@ -38,22 +38,5 @@ $( function () {
     return date;
   };
 
-  // Returns first date that should NOT be drawn by the calendar
-  // We can do a < comparison.
-  var createVisualEndDate = function ( date ) {
-    date = date || new Date();
-    var month = date.getMonth();
-
-    date.setDate( 28 );
-    while ( date.getMonth() === month ) {
-      date.setDate( date.getDate() + 1 );
-    }
-    while ( date.getDay() !== 0 ) {
-      date.setDate( date.getDate() + 1 );
-    }
-
-    return date;
-  };
-
   draw();
 } );
