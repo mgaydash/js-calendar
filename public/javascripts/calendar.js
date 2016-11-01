029d8c6929e8c9d531226f32f5c54ba092d80767
mgaydash <mgaydash@dataforma.com>
2016-11-01 10:03:59 -0400

    Simplify draw logic. Perform jQuery safety check

--- public/javascripts/calendar.js
+++ public/javascripts/calendar.js
@@ -1,29 +1,27 @@
 $( function () {
   "use strict";
 
+  var $ = window.$ || ( function () { throw "jQuery not found"; } )(); 
   var elements = {};
   elements.cell = $('.item').detach();
   elements.row = $('.container').detach();
-
-  var createWeekRow = function () {
-    var i;
-    var row = elements.row.clone();
-
-    for ( i = 0; i < 7; i++ ) {
-      row.append( elements.cell.clone() );
-    }
-
-    return row;
-  };
 
   var draw = function () {
     var date = createVisualStartDate();
     var month = new Date().getMonth();
     var i;
+    var row;
+    var cell;
 
     while ( date.getMonth() <= month ) {
-      $( 'body' ).append( createWeekRow() );
-      date.setDate( date.getDate() + 7 );
+      row = elements.row.clone();
+      for ( i = 0; i < 7; i++ ) {
+        cell = elements.cell.clone();
+        cell.html( date.getDate() );
+        row.append( cell );
+        date.setDate( date.getDate() + 1 );
+      }
+      $( 'body' ).append( row );
     }
   };
 
